b073034b6a9fb93a59c4c99c8a6c08bc624cf9c6
Bhagyashri Yogesh Patil <bhagyashriyogeshpatil@gmail.com>
2024-07-24 13:08:42 +0000

    Setup database and environment variables for Django deployment in settings.py

--- flavorfusion/settings.py
+++ flavorfusion/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4+dh-bs0@eals7iw&t$%delrq7rrb)ulai4)gr@h@nq86zwh%m'
+# SECRET_KEY = 'django-insecure-4+dh-bs0@eals7iw&t$%delrq7rrb)ulai4)gr@h@nq86zwh%m'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
