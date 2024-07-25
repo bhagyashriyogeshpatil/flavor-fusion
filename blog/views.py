caa8355e7a51a96cf792461e985957836b49e1fb
Bhagyashri Yogesh Patil <bhagyashriyogeshpatil@gmail.com>
2024-07-25 11:06:21 +0000

    create base template

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 # Create your views here.
-def my_blog(request):
-    return HttpResponse("Hello, Blog!")
+
+class Index(TemplateView):
+    template_name = "blog/index.html"
