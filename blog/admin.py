a0a39d735835725efa5d8a4128b254653799b61b
Bhagyashri Yogesh Patil <bhagyashriyogeshpatil@gmail.com>
2024-07-31 13:50:45 +0000

    Install Summernote to enhance admin panel for easier blog post creation and editing

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 from .models import CuisineType, Recipe
+from django_summernote.admin import SummernoteModelAdmin
+
+@admin.register(Recipe)
+class RecipeAdmin(SummernoteModelAdmin):
+    list_display = ('title', 'slug', 'status', 'author', 'created_on')
+    search_fields = ['title', 'cuisines_type', 'status']
+    list_filter = ('status', 'created_on',)
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('ingredients', 'instructions')
 
 # Register your models here.
 admin.site.register(CuisineType)
-admin.site.register(Recipe)
